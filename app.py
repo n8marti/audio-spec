c8554d6d347053f9530be75250124bcde42b6c62
Nate Marti <nate_marti@sil.org>
2021-03-25 16:26:31 +0100

    don't convert to frames too soon

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Read and parse speech info from audio files."""
 
-import numpy as np
 import sys
 
 from pathlib import Path
@@ -23,7 +22,9 @@ if __name__ == '__main__':
         file_info, byte_frames = utils.get_wav_info(input_file)
 
     startfr = None
+    startsec = 0
     endfr = None
+    endsec = None
     if len(sys.argv) > 2:
         endsec = float(sys.argv[-1])
         endfr = int(endsec * file_info.framerate) # * file_info.sampwidth)
@@ -32,7 +33,7 @@ if __name__ == '__main__':
         startfr = int(startsec * file_info.framerate) # * file_info.sampwidth)
 
     # Convert byte_frames to np_frames, crop according to start and end args.
-    np_frames = utils.convert_to_np_frames(byte_frames, startfr, endfr)
+    np_frames = utils.convert_to_np_frames(byte_frames)
 
     # Generate illustrative plots; return spectrum.
     #np_spectrum, np_freqs, np_times = outputs.generate_plots(input_file, np_frames, file_info.framerate)
@@ -40,15 +41,14 @@ if __name__ == '__main__':
     # Normalize specturm by applying filters.
     np_spectrum, np_freqs = filters.normalize_spectrum(np_spectrum, np_freqs, np_times)
     # Organize time frame data into dictionary.
-    time_frames = analyzers.get_time_frames(np_spectrum, np_freqs, np_times)
-    #for t, d in time_frames.items():
-    #    print(f"{t}:\t{d}")
+    time_frames = analyzers.get_time_frames(np_spectrum, np_freqs, np_times, startsec, endsec)
+
+    #outputs.print_sample_properties(analyzers.get_sample_properties(time_frames))
     #outputs.print_terminal_spectrogram(np_spectrum, np_freqs, np_times, time_frames)
-    outputs.print_frame_data(time_frames, startsec)
+    analyzers.get_phonemes(time_frames)
+    #outputs.print_frequencies(np_freqs)
+    #outputs.print_amplitudes(time_frames)
+    print()
+    outputs.print_frame_data(time_frames)
+
     exit()
-
-    phonemes = analyzers.get_phonemes(time_frames)
-    print(phonemes)
-
-    # Output descriptive properties of spectrum.
-    outputs.print_spectrum_properties(np_spectrum, np_freqs, np_times)
